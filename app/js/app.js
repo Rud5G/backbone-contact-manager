5685c9bfc758a2e6e301f7e9edf4ead47c1f73fe
Rudger Gravestein <r.gravestein@triple-networks.com>
2020-02-02 16:25:02 +0100

    Add step-9

--- app/js/app.js
+++ app/js/app.js
@@ -5,12 +5,58 @@ window.ContactManager = {
 
 
   start: function(data) {
-    console.log('Contact Manager started!');
-
     var contacts = new ContactManager.Collections.Contacts(data.contacts);
-    var contactsView = new ContactManager.Views.Contacts({
-      collection: contacts
+    var router = new ContactManager.Router();
+
+    router.on('route:home', function() {
+      router.navigate('contacts', {
+        trigger: true,
+        replace: true
+      });
     });
-    $('.main-container').html(contactsView.render().$el);
+
+    router.on('route:showContacts', function() {
+      var contactsView = new ContactManager.Views.Contacts({
+        collection: contacts
+      });
+
+      $('.main-container').html(contactsView.render().$el);
+    });
+
+    router.on('route:newContact', function() {
+      var newContactForm = new ContactManager.Views.ContactForm({
+        model: new ContactManager.Models.Contact()
+      });
+
+      newContactForm.on('form:submitted', function(attrs) {
+        attrs.id = contacts.isEmpty() ? 1 : (_.max(contacts.pluck('id')) + 1);
+        contacts.add(attrs);
+        router.navigate('contacts', true);
+      });
+
+      $('.main-container').html(newContactForm.render().$el);
+    });
+
+    router.on('route:editContact', function(id) {
+      var contact = contacts.get(id);
+      var editContactForm;
+
+      if (contact) {
+        editContactForm = new ContactManager.Views.ContactForm({
+          model: contact
+        });
+
+        editContactForm.on('form:submitted', function(attrs) {
+          contact.set(attrs);
+          router.navigate('contacts', true);
+        });
+
+        $('.main-container').html(editContactForm.render().$el);
+      } else {
+        router.navigate('contacts', true);
+      }
+    });
+
+    Backbone.history.start();
   }
 };
